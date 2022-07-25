36e64a3b0c652505117695187f87e32196bf4330
gools11 <106004789+gools11@users.noreply.github.com>
2022-07-25 10:21:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="f8bfb621-6728-4a6c-ae69-2106cd3d7c8a"
+  Vless_UUID="6a0acd35-6d5b-42b2-b9e1-ffd0112686a8"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="f8bfb621-6728-4a6c-ae69-2106cd3d7c8a"
+  Vmess_UUID="6a0acd35-6d5b-42b2-b9e1-ffd0112686a8"
 fi
 echo ${Vmess_UUID}
 
